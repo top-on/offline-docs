8c92b344696bcee79471b439b1433700e3fda954
top-on <top-on@posteo.de>
2022-01-10 21:14:18 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # offline-docs-py
+
 Grab docs of project dependencies for offline availability (ALPHA)
---
+++ tests/test_offline_docs_py.py
@@ -0,0 +1,5 @@
+from offline_docs_py import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
