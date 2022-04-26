ea087ff443d12141502f36636f7fe234b06bc8da
top-on <top-on@posteo.de>
2022-04-26 21:32:47 +0200

    add cli test

--- tests/offline_docs/test_cli.py
+++ tests/offline_docs/test_cli.py
@@ -1,9 +1,14 @@
 """Functional tests of CLI."""
 
 
-from offline_docs.cli import python
+from offline_docs.cli import clean, python
 
 
 def test_python():
-    """Run python command of CLI."""
+    """Run 'python' command of CLI."""
     python()
+
+
+def test_clean():
+    """Run 'clean' command of CLI."""
+    clean()
